d0f712ee3a86d95a369d0a28b4d22cf89b08020b
luisantonioig <c-remix@hotmail.es>
2016-11-01 16:28:10 -0600

    Cambio de texto

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 browser = webdriver.Chrome()
 browser.get('http://localhost:8000')
-assert 'To-do' in browser.title , "El titulo del buscador es " + browser.title
+assert 'To-do' in browser.title , "El titulo de la página es " + browser.title
 browser.quit()
