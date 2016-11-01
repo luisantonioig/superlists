141668f449cb1ba94fd4b7b5776229cfb4c75152
luisantonioig <c-remix@hotmail.es>
2016-11-01 16:23:16 -0600

    Resultado en la prueba

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 browser = webdriver.Chrome()
 browser.get('http://localhost:8000')
-assert 'To-do' in browser.title
+assert 'To-do' in browser.title , "El titulo del buscador es " + browser.title
 browser.quit()
