b86faacb7ed7ba6502875e605c4db915fd6b8b18
luisantonioig <c-remix@hotmail.es>
2016-11-02 14:12:26 -0600

    Una clase de pruebas funcionales

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,24 @@
 from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'To-do' in browser.title , "El titulo de la página es " + browser.title
-browser.quit()
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrive_it_later(self):
+		# Edit has heard about a cool new online to-do app. She goes
+		# to check out its homepage
+		self.browser.get('http://localhost:8000')
+
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-do',self.browser.tittle)
+		self.fail('Finish the test!')
+
+		# She is invited to enter a to-do item straight away
+		[...rest of comments as before]
+
+if __name__ == '__main__':
+	unittes.main(warnings='ignore')
